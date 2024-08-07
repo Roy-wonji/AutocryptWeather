18c488877b3c33ee5eb0fd4210c8566c42f1fbd1
Roy <shuwj81@daum.net>
2024-08-08 01:22:39 +0900

    ✨[feat]: 네트워킹 공용 부분 구현 #2
    * DiConatiner 구현

---
+++ AutocryptWeather/Projects/App/Sources/Di/AppDIContainer.swift
@@ -0,0 +1,74 @@
+//
+//  AppDIContainer.swift
+//  Weather
+//
+//  Created by 서원지 on 8/8/24.
+//
+
+import Foundation
+import UseCase
+import DiContainer
+
+public final class AppDIContainer {
+    public static let shared: AppDIContainer = .init()
+
+    private let diContainer: DependencyContainer = .live
+
+    private init() {
+    }
+
+    public func registerDependencies() async {
+        await registerRepositories()
+        await registerUseCases()
+    }
+
+    // MARK: - Use Cases
+    private func registerUseCases() async {
+//        await registerAuthUseCase()
+//        await registerSignUpUseCase()
+
+//        await registerQrCodeUseCase()
+    }
+
+//    private func registerAuthUseCase() async {
+//        await diContainer.register(AuthUseCaseProtocol.self) {
+//            guard let repository = self.diContainer.resolve(AuthRepositoryProtocol.self) else {
+//                assertionFailure("AuthRepositoryProtocol must be registered before registering AuthUseCaseProtocol")
+//                return AuthUseCase(repository: DefaultAuthRepository())
+//            }
+//            return AuthUseCase(repository: repository)
+//        }
+//    }
+    
+//    private func registerSignUpUseCase() async {
+//        await diContainer.register(SignUpUseCaseProtocol.self) {
+//            guard let repository = self.diContainer.resolve(SignUpRepositoryProtocol.self) else {
+//                assertionFailure("SignUpRepositoryProtocol must be registered before registering SignUpUseCaseProtocol")
+//                return SignUpUseCase(repository: DefaultSignUpRepository())
+//            }
+//            return SignUpUseCase(repository: repository)
+//        }
+//    }
+
+
+    // MARK: - Repositories Registration
+    private func registerRepositories() async {
+//        await registerAuthRepositories()
+//        await registerSignUpRepositories()
+
+    }
+
+//    private func registerAuthRepositories() async {
+//        await diContainer.register(AuthRepositoryProtocol.self) {
+//            AuthRepository()
+//        }
+//    }
+//    
+//    private func registerSignUpRepositories() async {
+//        await diContainer.register(SignUpRepositoryProtocol.self) {
+//            SingUpRepository()
+//        }
+//    }
+    
+}
+
